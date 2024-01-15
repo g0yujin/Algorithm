32f85b27560d18ae34ddfd4dbd3b28d6405d7e26
g0yujin <136612437+g0yujin@users.noreply.github.com>
2024-01-15 22:13:31 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,13 +1,13 @@
 h, m = map(int, input().split())
 
-if h == 0:
-    h = (60*24 + m -45) // 60
-else:
-    h = (60*h + m -45) // 60
+if m >= 45 :
+    m = m - 45
     
-m = (60*h + m -45) % 60
-
-if h == 24:
-    print('0',m)
+else :
+    m = 60 + m - 45
+    if h == 0 :
+        h = 24 - 1
+    else : 
+        h = h -1
     
-else : print(h,m)
+print (h,m)
