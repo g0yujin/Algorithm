0b442ae3775e4f3d0b4b09b4235aea2e3d9a8f20
g0yujin <136612437+g0yujin@users.noreply.github.com>
2024-01-26 18:09:52 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -2,17 +2,11 @@ import sys
 
 N, M = map(int, input().split())
 
-bucket = []
-
-for i in range(N):
-    bucket.append(0)
+bucket = [0] * N
 
 for i in range(M):
     I, J, K = map(int, sys.stdin.readline().split())
-    ball = []
-    for i in range(J-I+1):
-        ball.append(K)
-
-    bucket[I - 1:J ] = ball
+    for x in range(I,J+1):
+        bucket[x-1] = K
 
 print(*bucket)
