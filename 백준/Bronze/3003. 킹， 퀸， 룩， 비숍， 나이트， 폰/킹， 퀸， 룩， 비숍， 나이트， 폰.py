cf603b2c9e13eb0fcc19b3d8d1177354db130ce6
g0yujin <136612437+g0yujin@users.noreply.github.com>
2024-01-30 21:56:30 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,12 +1,10 @@
 import sys
 
-chess = list(map(int, sys.stdin.readline().split()))
+chess = [1, 1, 2, 2, 2, 8]
+have = list(map(int, sys.stdin.readline().split()))
 
-chess[0] = 1 - chess[0]
-chess[1] = 1 - chess[1]
-chess[2] = 2 - chess[2]
-chess[3] = 2 - chess[3]
-chess[4] = 2 - chess[4]
-chess[5] = 8 - chess[5]
+find = []
+for ci,hi in zip(chess,have):
+    find.append(ci-hi)
 
-print(*chess)
+print(*find)
